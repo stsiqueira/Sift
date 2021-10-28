a48b40784654af309151241b175d8411dab1c2c2
Thiago <stsiqueira@outlook.com>
2021-10-27 17:35:04 -0700

    style home screen #11
    style home screen #11

--- App.js
+++ App.js
@@ -9,13 +9,30 @@ import Camera from './src/mainScreens/Camera';
 import RCL from './src/mainScreens/RCL';
 import Profile from './src/mainScreens/Profile';
 import Login from './src/mainScreens/login';
+import { useFonts }from 'expo-font';
+
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false)
+  const [fontsLoaded] = useFonts({
+    'Lato-Black': require('./src/assets/fonts/Lato-Black.ttf'),
+		'Lato-BlackItalic': require('./src/assets/fonts/Lato-BlackItalic.ttf'),
+		'Lato-Bold': require('./src/assets/fonts/Lato-Bold.ttf'),
+		'Lato-BoldItalic': require('./src/assets/fonts/Lato-BoldItalic.ttf'),
+		'Lato-LightItalic': require('./src/assets/fonts/Lato-LightItalic.ttf'),
+		'Lato-Italic': require('./src/assets/fonts/Lato-Italic.ttf'),
+		'Lato-Light': require('./src/assets/fonts/Lato-Light.ttf'),
+		'Lato-Regular': require('./src/assets/fonts/Lato-Regular.ttf'),
+		'Lato-Thin': require('./src/assets/fonts/Lato-Thin.ttf'),
+		'Lato-ThinItalic': require('./src/assets/fonts/Lato-ThinItalic.ttf'),
+  })
+  if(!fontsLoaded) {
+		return null;
+	}
   return (
     <NavigationContainer>
-      {loggedIn ?
+      {!loggedIn ?
 
           <Tab.Navigator>
             <Tab.Screen name="Home" component={Home} />
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from "react-native";
+
+const darkBlue = '#134075'
+export const globalStyles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        width:'100%',
+        backgroundColor: '#F3F4F8',
+        paddingHorizontal:20,
+        paddingVertical:30,
+      },
+    callToActionWrapper:{
+        paddingBottom:30,
+    },
+    promotionSectionWrapper:{
+        paddingBottom:34,
+    },
+
+    CallToActionContainer:{
+        marginTop:10,
+        flexDirection:"row",
+    },
+    CallToActionImageContainer:{
+        padding: 24,
+        backgroundColor: '#fff',
+        borderWidth:1,
+        borderColor:'#E4E6EE',
+        borderTopLeftRadius:5,
+        borderBottomLeftRadius:5
+    },
+    CallToActionTextButtonContainer:{
+        backgroundColor: darkBlue,
+        borderTopRightRadius:5,
+        borderBottomRightRadius:5,
+        flex:1,
+        paddingTop:14,
+        paddingLeft:14,
+        justifyContent:'space-between'
+    },
+    CallToActionText:{
+        fontSize:14,
+        lineHeight:20,
+        color:'white'
+    },
+    CallToActionButtonContainer:{
+        flexDirection: "row",
+        justifyContent:"flex-end",
+        paddingBottom:8,
+        paddingRight:10
+    },
+    screenHeading:{
+        fontFamily:"Lato-Bold",
+        fontSize:36,
+        lineHeight:43,
+        marginBottom:14
+    },
+    sectionHeading:{
+        fontFamily:"Lato-Bold",
+        fontSize:24,
+        lineHeight:29
+    }
+})
